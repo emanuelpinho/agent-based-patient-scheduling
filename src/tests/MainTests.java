d1cdcfa05e0b560a3b49ba4a3cfdda54f5c65aef
emanuelpinho <emanuelaspinho@gmail.com>
2014-11-19 13:17:25 +0000

    jUnit tests

---
+++ src/tests/MainTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTests {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
